e29bb6898775ae32885845db6e9b3fe67c2635a6
naegonggae <https://github.com/naegonggae/>
2022-10-24 00:54:46 +0900

    first commit 1. parser 2. hospital 3. test 4. FileControler 설정 등 5. HospitalParser에 contain"마디"추가 6. subdivision 추가

--- parser/HospitalParser.java
+++ parser/HospitalParser.java
@@ -4,11 +4,14 @@ import com.lion.domain.Hospital;
 
 public class HospitalParser implements Parser<Hospital> {
     private String getSubdivision(String name) {
-        String[] subdivisions = {"소아", "피부", "성형외", "정형외과",
+        String[] subdivisions = {"소아", "피부", "성형외", "정형외과", "이비인후과",
                 "산부인과", "관절", "안과", "가정의학과", "비뇨기과", "치과","내과", "외과"};
         for (String subdivirion : subdivisions) {
             if (name.contains(subdivirion)) {
                 return subdivirion;
+            }
+            if (name.contains("마디")) {
+                return "정형외과";
             }
         }
         return "";
